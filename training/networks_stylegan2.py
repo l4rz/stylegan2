b83d9832bf9bcfc474b655787494a4018bb364ea
mike <perfect1@gmail.com>
2020-03-25 13:52:57 +0100

    enabled

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -500,7 +500,7 @@ def G_synthesis_stylegan2(
     for res in range(3, resolution_log2 + 1):
         with tf.variable_scope('%dx%d' % (2**res, 2**res)):
             x = block(x, res)
-            if 2**res == 64 and False:
+            if 2**res == 64:
                 print('Adding self-attention block to generator')
                 #x = non_local_block(x, "SelfAtten", use_sn=True)
                 x = apply_bias_act(non_local_block(x, "SelfAtten", use_sn=True), act=act)
@@ -674,7 +674,7 @@ def D_stylegan2(
         with tf.variable_scope('%dx%d' % (2**res, 2**res)):
             if architecture == 'skip' or res == resolution_log2:
                 x = fromrgb(x, y, res)
-            if 2**res == 64 and False:
+            if 2**res == 64:
                 print('Adding self-attention block to discriminator')
                 #x = non_local_block(x, "SelfAtten", use_sn=True)
                 x = apply_bias_act(non_local_block(x, "SelfAtten", use_sn=True), act=act)
